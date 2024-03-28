09833a53134e3fc6300a349a48f6136dbcb63339
Virginia Gichira <gichiravirginia@gmail.com>
2024-03-28 15:29:26 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun( activity = "roller-skate"){
+    //const activity = "roller-skate";
+    return `This Saturday, I want to ${activity}!`;
+}
+
+ function mondayWork(task = "go to the office") {
+//    console.log(mondayWork());
+  return `This Monday, I will ${task}.`;
+}
+
+// index.js
+function wrapAdjective(symbol) {
+  return function(adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+  }
+}
